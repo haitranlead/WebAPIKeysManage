f09c83e855402b6cb51e33288af2783e6181ec4f
haitranlead <haitranlead@gmail.com>
2024-07-11 15:39:23 +0700

    commit

--- Controllers/KeyManagementAPIController.cs
+++ Controllers/KeyManagementAPIController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using WebAPIKeysManage.Models;
 
@@ -27,7 +26,6 @@ namespace WebAPIKeysManage.Controllers
         {
             return Ok(keys);
         }
-
 
         [HttpGet("validate/{key}")]
         public IActionResult ValidateKey(string key)
